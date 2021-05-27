e77b7a969b7aa0f9d3154201fcd49861d8e5e460
Marta SH <msastreharo@gmail.com>
2021-05-27 11:29:51 +0200

    Initial commit

---
+++ src/test/java/org/factoriaf5/usercrud/UsercrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.usercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
